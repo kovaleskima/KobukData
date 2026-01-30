5b31c8671e6fd3c3d7886cba9e99a052544ec432
Maddy Kovaleski <makov@Puter.local>
2026-01-30 12:30:26 -0800

    converted to lat/lon dicts, stored in 'Sentinel2_cracks_processed'

--- preprocessing.py
+++ preprocessing.py
@@ -11,9 +11,8 @@ output_dir.mkdir(exist_ok=True)
 
 def convert_to_lat_lon(raw_path):
 
-    with open(raw_path) as f:
-        c_dat = json.load(f)
-        #chain_data_file_printer(c_dat, 0)
+    with open(raw_path) as fin:
+        c_dat = json.load(fin)
 
 
     # Load the SAME NetCDF used in NCCut
@@ -26,25 +25,31 @@ def convert_to_lat_lon(raw_path):
 
     chain_points = {}
 
-    for chain_name, chain in c_dat.items():
-        points_list = []  # list of arrays for this chain
-        
-        # Collect all cuts
-        for cut_name, cut in chain.items():
-            if cut_name.startswith("Cut"):
-                x = np.array(cut['x'])
-                y = np.array(cut['y'])
-                points_list.append(np.vstack([y, x]))  # shape (2, N_cut)
-        
-        if points_list:  # make sure there are cuts
-            coords = np.hstack(points_list)  # shape (2, total_points)
-            
-            lat_transect = map_coordinates(lat, coords, order=1)
-            lon_transect = map_coordinates(lon, coords, order=1)
-            
-            # save as (2, total_points) array: first row lat, second row lon
-            chain_points[chain_name] = np.vstack([lat_transect, lon_transect])
+    for metadata_name, metadata in c_dat.items():
 
+        for chain_name, chain in metadata.items():
+            points_list = []
+            # Get actual inline chain objects
+            if isinstance(metadata[chain_name], dict): 
+                for cut_name, cut in chain.items():
+                    if cut_name.startswith("Cut"):
+                        x = np.array(cut['x'])
+                        y = np.array(cut['y'])
+                        points_list.append(np.vstack([y, x]))  # shape (2, N_cut)
+                
+                if points_list:  # make sure there are cuts
+                    coords = np.hstack(points_list)  # shape (2, total_points)
+                    
+                    lat_transect = map_coordinates(lat, coords, order=1)
+                    lon_transect = map_coordinates(lon, coords, order=1)
+                    
+                    # save as (2, total_points) array: first row lat, second row lon
+                    chain_points[chain_name] = {"lat": lat_transect.tolist(), "lon": lon_transect.tolist()}
+    
+    # Save chain_points to its own json
+    processed_path = str(output_dir) + '/' + str(filename)
+    with open(processed_path, "w") as fout:
+        json.dump(chain_points, fout, indent=4)
         
 # Batch conversion
 for json_path in sorted(input_dir.glob("*.json*")):
